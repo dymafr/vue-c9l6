5fb1d91edbbde18165616417aad488ef2c6df6be
dymafr <erwan1309@hotmail.com>
2022-03-08 15:05:31 +0100

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,20 @@
 </template>
 
 <script setup lang="ts">
+import { provide } from 'vue';
 import Blog from './Blog.vue';
+
+export interface AuthorInterface {
+  author: string;
+  birthdate: number;
+}
+
+const author: AuthorInterface = {
+  name: 'Chateaubriand',
+  birthdate: 1768,
+};
+
+provide<AuthorInterface>('author', author);
 </script>
 
 <style scoped lang="scss"></style>
--- src/Author.vue
+++ src/Author.vue
@@ -1,5 +1,12 @@
-<template>Test</template>
+<template>
+  <small> {{ author.name }} - {{ author.birthdate }}</small>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { inject } from 'vue';
+import { AuthorInterface } from './App.vue';
+
+const author = inject<AuthorInterface>('author')!;
+</script>
 
 <style scoped lang="scss"></style>
